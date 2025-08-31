d5f714a57b687633706602a0acd8344c0fc5d91a
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-01 08:33:36 +0900

    ユーザー情報更新時のエラーの実装とテストの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,8 +20,18 @@ class UsersController < ApplicationController
     end
   end
 
-   def edit
+  def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   private
