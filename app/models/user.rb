2adad06266e1cbc20776e4b7670eee7890a1190e
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-01 16:01:19 +0900

    Chapter11 演習

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # アカウントを有効にする
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
     # メールアドレスをすべて小文字にする
@@ -59,4 +70,6 @@ class User < ApplicationRecord
       self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
+
+
 end
