be5896ee5968414e591d584f37653f2476dbc855
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-01 14:32:33 +0900

    ユーザーモデルの定義にaccount_activationを追加 Fixes #7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_01_010327) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_01_052119) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_01_010327) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
