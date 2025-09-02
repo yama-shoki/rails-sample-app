7bc2a79bdf75554cbead74fa6f8700d51d128cbe
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-02 15:04:05 +0900

    マイクロポストのモデル・シード・一覧画面を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_02_005218) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_02_053829) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -26,4 +35,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_02_005218) do
     t.datetime "reset_sent_at"
   end
 
+  add_foreign_key "microposts", "users"
 end
