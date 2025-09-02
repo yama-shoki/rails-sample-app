a119106d9384442062e11fca089974080677bbba
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-02 16:47:03 +0900

    画像のアップロード機能を実装

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -69,4 +69,16 @@ class MicropostSidebarTest < MicropostsInterface
     get root_path
     assert_match "1 micropost", response.body
   end
+
+  test "should have a file input field for images" do
+    get root_path
+    assert_select 'input[type=file]'
+  end
+
+  test "should be able to attach an image" do
+    cont = "This micropost really ties the room together."
+    img  = fixture_file_upload('kitten.jpg', 'image/jpeg')
+    post microposts_path, params: { micropost: { content: cont, image: img } }
+    assert assigns(:micropost).image.attached?
+  end
 end
