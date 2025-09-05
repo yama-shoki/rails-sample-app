50254aa1a1d5b4c84d08a482bd4f9d514d1bba4c
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-05 15:43:21 +0900

    deploymentフラグを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.19.0"
-
 set :application, "sample_app"
 set :repo_url, "git@github.com:yama-shoki/rails-sample-app.git"
-# set :rbenv_ruby, File.read('.ruby-version').strip  ← コメントアウト済み
 set :branch, ENV['BRANCH'] || "main"
-
-# rbenvを無効化してシステムRubyを使用
-# set :rbenv_type, :system
-# set :rbenv_ruby, 'system'
 
 # Nginxの設定ファイル名と置き場所を修正
 set :nginx_config_name, "#{fetch(:application)}.conf"
@@ -17,6 +11,7 @@ set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
 append :linked_files, "config/master.key"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_modules"
 
-# 既存の設定の下に追加
-set :bundle_flags, '--deployment --quiet'
+# bundler設定（deploymentフラグを削除）
+set :bundle_flags, '--quiet'
+set :bundle_without, %w{development test}.join(' ')
 set :bundle_env_variables, { 'BUNDLE_IGNORE_RUBY_VERSION' => '1' }
