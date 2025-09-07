717ad984ca1cf8f2a099e5adef9785e8009b7d9b
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-08 01:06:52 +0900

    サインアップメールが飛ばせるように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Mailgun SMTP設定
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mailgun.org',
+    port: 587,
+    domain: Rails.application.credentials.mailgun[:domain],
+    user_name: Rails.application.credentials.mailgun[:smtp_username],
+    password: Rails.application.credentials.mailgun[:smtp_password],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  # メール送信元設定
+  config.action_mailer.default_options = {
+    from: Rails.application.credentials.mailgun[:smtp_username]
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
