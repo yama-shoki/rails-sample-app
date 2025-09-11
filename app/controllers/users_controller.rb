7bb9ac55f754a7f0d233db4bfe27b71e10808c25
Shoki Yamamoto <radyama0905@gmail.com>
2025-09-11 12:34:11 +0900

    ログイン時に/signUpへのアクセスを/にリダイレクトするよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy,
                                         :following, :followers]
+  before_action :already_logged_in, only: [:new, :create]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
